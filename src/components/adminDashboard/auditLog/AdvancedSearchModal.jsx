da24cc37fb7280f0b6e6106e38eeeb25018f580d
Agonsi123 <agonsiudodirim@gmail.com>
2025-05-13 15:38:01 +0100

    edited Advance search

--- src/components/adminDashboard/auditLog/AdvancedSearchModal.jsx
+++ src/components/adminDashboard/auditLog/AdvancedSearchModal.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from 'react';
+import advanceFilter from '../../../assets/advanceFilter.svg';
 
 const AdvancedSearchModal = () => {
   return (
-    <div>
-        Advanced Search
+    <div className="bg-blue-200 absolute flex flex-col gap-2 py-7 px-4 top-0 right-5">
+      <div>
+        <img src={advanceFilter} alt="Filter Icon" />
+        <h6>Advance search and filter</h6>
+      </div>
     </div>
-  )
+  );
 }
 
 export default AdvancedSearchModal
--- src/components/adminDashboard/auditLog/AuditSearchSection.jsx
+++ src/components/adminDashboard/auditLog/AuditSearchSection.jsx
@@ -1,12 +1,14 @@
-import React, {useState} from 'react';
-// import AdvancedSearchModal from './AdvancedSearchModal';
+import React, {useContext, useState} from 'react';
+import AdvancedSearchModal from './AdvancedSearchModal';
+import { AppContext } from '../../../context/Index';
 
 const AuditSearchSection = ({tittle, icon, icon2}) => {
+  const {showAdvanceSearch, setShowAdvanceSearch} = useContext(AppContext);
 
   // const[isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className="py-3 px-7 flex justify-between  items-center w-full bg-[#f8f8f8]">
+    <div className="py-3 px-7 flex justify-between  items-center w-full bg-[#f8f8f8] relative">
       <h1 className="w-full font-sanns font-medium text-xl">{tittle}</h1>
       <div className="flex gap-3">
         <div className="flex justify-between items-center border-[1px] py-1 px-4 border-[#00000026] w-[27rem] rounded">
@@ -20,14 +22,16 @@ const AuditSearchSection = ({tittle, icon, icon2}) => {
               className=" placeholder:text-[#00000080] placeholder:font-medium placeholder:text-base"
             />
           </div>
-          <div className=""
-          // onClick={setIsOpen((prev) => !prev)}
+          <div
+            className="cursor-pointer"
+            // onClick={setShowAdvanceSearch((prev) => !prev)}
+            // onClick={setShowAdvanceSearch(true)}
           >
             {icon2}
           </div>
+          {showAdvanceSearch && <AdvancedSearchModal />}
         </div>
       </div>
-      {/* {isOpen && <AdvancedSearchModal/>} */}
     </div>
   );
 }
